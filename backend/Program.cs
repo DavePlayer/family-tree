179b04a0bdcd93ad599f594e08313e1fd211c3c7
Dave Player <daveplayer2001@gmail.com>
2024-01-23 15:57:06 +0100

    updated dockerfile to just work XD

--- backend/Program.cs
+++ backend/Program.cs
@@ -73,8 +73,13 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "Your API V1");
+        c.RoutePrefix = "swagger";
+    });
 }
+
 app.UseCors("CorsPolicy");
 app.UseAuthentication();
 app.UseAuthorization();
