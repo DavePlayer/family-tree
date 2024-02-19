6c8c89cf1d5440a30ac3947a7c5119e113f81d17
DavePlayer <davepalyer2001@gmail.com>
2024-02-19 15:16:25 +0100

    added readme file

--- frontend/src/routes/login/components/GradientText.tsx
+++ frontend/src/routes/login/components/GradientText.tsx
@@ -1,13 +1,9 @@
 export const GradientText = () => {
     return (
         <div className="w-8/12 flex flex-col">
-            <h1 className="login-title text-center mb-3">Jakiś fajny text</h1>
+            <h1 className="login-title text-center mb-3">Create your family tree</h1>
             <h2 className="login-subtitle">
-                Lorem, ipsum dolor sit amet consectetur adipisicing elit. Sunt quia sed esse iusto,
-                vel cumque blanditiis quasi ullam modi explicabo, nisi quo commodi consectetur aut
-                beatae laborum voluptatibus nesciunt ducimus. Minus ratione quaerat est error
-                placeat fugiat, molestiae excepturi ab nostrum nesciunt voluptates voluptatibus ea
-                officiis numquam. Incidunt, deserunt dolores!
+                Take and use this application, everyone. It is a student project that evidently operates in full functionality. For those who wish to organize their tree, this will be a facilitation.
             </h2>
         </div>
     );
